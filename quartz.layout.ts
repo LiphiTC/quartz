3f2d67dbc3e33594946f51b84ade88172e6b8b1c
Liphi <iprogcen@hotmail.com>
2025-05-12 11:54:08 +0300

    Added custom favicon

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Source": "https://code.pepega.club/MireaUVSystems/Obsidian",
+      "Очко барана": "https://baran.liphi.co"
     },
   }),
 }
